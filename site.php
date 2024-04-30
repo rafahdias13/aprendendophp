7b50592633591b2517f59419a7acedf9fa6ea03f
rafaeldias13 <rafael.dias.imagemproducoes@gmail.com>
2024-04-30 17:17:44 -0300

    aula dia 30 site

---
+++ site.php
@@ -0,0 +1,13 @@
+<?php
+$nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
+$endereco = $_POST['endereco'];
+
+
+echo $nome;
+echo '<p>Você mora na rua</p>';
+echo $endereco;
+echo '<p>seu cpf</p>';
+echo $cpf;
+
+?>
